9ebe0ead0536aa86c09a095de6111b90de80a00e
phuongwj <phamjulia9@gmail.com>
2026-07-29 14:54:26 -0300

    feat: group crud endpoints

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,6 +6,7 @@ import aiRoutes from "./ai/aiRoutes.ts";
 import cookieParser from "cookie-parser";
 import authRoutes from "./auth/authRoutes.ts";
 import transactionRoutes from './transactions/transactionRoutes.ts';
+import groupRoutes from './groups/groupRoutes.ts';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -15,6 +16,7 @@ app.use(cookieParser());
 
 app.use('/api/auth', authRoutes);
 app.use('/api/transactions', transactionRoutes);
+app.use('/api/groups', groupRoutes);
 app.use('/api/ai', aiRoutes);
 
 app.listen(PORT, () => {
--- backend/src/middleware/authMiddleware.ts
+++ backend/src/middleware/authMiddleware.ts
@@ -11,7 +11,7 @@ export const requireAuth = (req: Request, res: Response, next: NextFunction) => 
   const token = authHeader.split(' ')[1];
 
   try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET as string) as { userId: string };
+    const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET as string) as { userId: string };
     req.userId = decoded.userId;
     next();
   } catch (err) {
