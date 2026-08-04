3338dda52aebfd12763f2903cdfe6f69ac4232de
phuongwj <phamjulia9@gmail.com>
2026-08-04 19:29:38 -0300

    fix: removed duplicated code

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -36,34 +36,6 @@ app.use((req, res, next) => {
 
   next();
 });
-const allowedOrigins = new Set([
-  "http://localhost:5173",
-  "http://127.0.0.1:5173",
-]);
-
-app.use((req, res, next) => {
-  const origin = req.headers.origin;
-
-  if (origin && allowedOrigins.has(origin)) {
-    res.setHeader("Access-Control-Allow-Origin", origin);
-    res.setHeader("Vary", "Origin");
-    res.setHeader("Access-Control-Allow-Credentials", "true");
-    res.setHeader(
-      "Access-Control-Allow-Headers",
-      "Content-Type, Authorization"
-    );
-    res.setHeader(
-      "Access-Control-Allow-Methods",
-      "GET, POST, PUT, PATCH, DELETE, OPTIONS"
-    );
-  }
-
-  if (req.method === "OPTIONS") {
-    return res.sendStatus(204);
-  }
-
-  next();
-});
 
 app.use(cors({
   origin: (origin, callback) => {
